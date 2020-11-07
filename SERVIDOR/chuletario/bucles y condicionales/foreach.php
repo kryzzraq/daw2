afa170003536375949c59748ba33b69669833942
kryzzraq <lauramartinv394@gmail.com>
2020-11-07 11:32:41 +0100

    gf

--- SERVIDOR/chuletario/bucles y condicionales/foreach.php
+++ SERVIDOR/chuletario/bucles y condicionales/foreach.php
@@ -1,10 +1,17 @@
 Pare recoger sólo los valores del array:
 
-    $array = [1,2,3,4]
+    $array = [1,2,3,4];
     foreach($array as $valor){
         echo $valor;        
     }
 
---> Esto imprimiría: 
+--> Esto imprimiría: 1 2 3 4   
 
 
+Para recoger los valores y las claves del array: 
+
+    $array = [1,2,3,4];
+    foreach($array as $indice => $valor){
+        echo "{$clave} => {$valor}";
+        print_r{$array};
+    }
