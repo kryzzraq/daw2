b99abfd272adeceff8c2a1cb51e43725f8facf25
Laura <lauramartinv394@gmail.com>
2020-12-03 08:12:15 +0100

    03-12

--- CLIENTE/tema4/Date/mesFindeRojo/func.js
+++ CLIENTE/tema4/Date/mesFindeRojo/func.js
@@ -1,4 +1,5 @@
 let tdMes = document.getElementById("mes")
+let divjs = document.getElementById("divjstabla")
 const MES = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
 let fecha = new Date()
 
@@ -8,11 +9,24 @@ const obtenerMesLetra = () => {
 }
 
 const pintarTabla = () => {
-    debugger
-    let tabla = `<table border='1'><tr><td colspan='7'>octubre</td></tr></table>` 
-    return tabla    
+    let tabla = ''
+    let contadorID = 0
+
+    tabla += `<table border="1"><th colspan="7">${obtenerMesLetra()}</th>`
+    for(let i = 0; i<7;i++){
+        tabla += '<tr>'
+        for(let k = 0; k<7;k++){
+            tabla +=`<td id="celda${contadorID}">Holi</td>`
+            contadorID++
+        }
+    tabla += '</tr>'
+    }
+    tabla += '</table>'
+    
+    return tabla
 }
 tdMes.innerHTML = obtenerMesLetra()
+divjs.innerHTML = pintarTabla()
 
 const obtenerDiaSemana = () =>{
     let diaSemana = fecha.getDay()
