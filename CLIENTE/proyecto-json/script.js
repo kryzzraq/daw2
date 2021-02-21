31dee3ea76525b73f26c70e5d7043755daa9c103
Laura <lauramartinv394@gmail.com>
2021-02-21 17:05:09 +0100

    gfgfgf

--- CLIENTE/proyecto-json/script.js
+++ CLIENTE/proyecto-json/script.js
@@ -3,9 +3,16 @@
         //Carga
         let response = await fetch('cv.json')
         let DatosJSON = await response.json();
+        //Parte izquierda
+        document.getElementById('textPerfil').innerHTML = DatosJSON.datos_basicos.descripcion
+            //idiomas
+            for(let i=0;i<DatosJSON.datos_basicos.idiomas.lenght;i++){
+                
+            }
+        //parte derecha
         document.getElementById('titNombre').innerHTML = DatosJSON.datos_basicos.nombre + " " + DatosJSON.datos_basicos.apellidos
         document.getElementById('titConcepto').innerHTML = DatosJSON.datos_basicos.concepto
-        document.getElementById('descripcion').innerHTML = DatosJSON.datos_basicos.descripcion
+        document.getElementById('descripcion').innerHTML = DatosJSON.datos_basicos.trabajo
         document.getElementById('emailText').innerHTML = DatosJSON.datos_basicos.email
         document.getElementById('telText').innerHTML = DatosJSON.datos_basicos.telefono
         document.getElementById('infoEstudios').innerHTML = mostrar()
